16399ba32711981c9f2074f2183b2055fa07f5c2
Ural Ataullin <ataullinural@gmail.com>
2023-09-04 13:48:48 +0500

    admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Specialist, User, SpecialistUser, Specialization
+from .models import (Specialist, User, SpecialistUser,
+                     Specialization, Gender, DemographicUser, Role)
 
 admin.site.register(Specialist)
+admin.site.register(Gender)
 admin.site.register(User)
+admin.site.register(DemographicUser)
 admin.site.register(SpecialistUser)
 admin.site.register(Specialization)
+admin.site.register(Role)
