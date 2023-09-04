5773d3f254d99e4d367f8fb174ebb1ae43a50715
Ural Ataullin <ataullinural@gmail.com>
2023-09-04 13:36:39 +0500

    add DemographicUser

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,17 +2,67 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-SPECIALIST_ROLE_CHOICES = (
-    ('trainer', 'Тренер'),
-    ('nutritionist', 'Диетолог')
-)
+class Role(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
+
+    def __str__(self):
+        return self.name
+
+
+class Gender(models.Model):
+    gender = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = 'Пол'
+        verbose_name_plural = 'Полы'
+
+    def __str__(self):
+        return self.gender
+
+
+class User(AbstractUser):
+    id = models.BigAutoField(primary_key=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=8, null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    is_spec = models.BooleanField()
+    status = models.CharField(max_length=64)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    edit_dt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+
+class DemographicUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    middle_name = models.CharField(max_length=128, null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    capture = models.BinaryField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Дополнительная информация о пользователе'
+        verbose_name_plural = 'Дополнительная информация о пользователях'
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
 
 
 class Specialization(models.Model):
-    """Представляет список возможных специализаций для специалистов.
-    Далее в SpecialistUser.specialization позволяет пользователям выбрать свою
-    специализацию, оставляя его пустым при универсальной регистрации."""
-
     name = models.CharField(max_length=70)
 
     class Meta:
@@ -23,55 +73,30 @@ class Specialization(models.Model):
         return self.name
 
 
-class User(AbstractUser):
-    _progress = models.IntegerField()
-    surname = models.CharField(max_length=70)
-    first_name = models.CharField(max_length=70)
-    patronymic = models.CharField(max_length=70)
-    email = models.EmailField(unique=True)
-    age = models.PositiveIntegerField()
-    gender = models.CharField(max_length=10)
-    weight = models.DecimalField(max_digits=5, decimal_places=2)
-    height = models.DecimalField(max_digits=4, decimal_places=2)
-    profile_photo = models.ImageField(
-        upload_to='profile_photos', null=True, blank=True)
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-    def __str__(self):
-        return f"{self.surname} {self.first_name}"
-
-
 class Specialist(models.Model):
-    id_special = models.AutoField(primary_key=True)
-    surname = models.CharField(max_length=70)
-    first_name = models.CharField(max_length=70)
-    patronymic = models.CharField(max_length=70)
-    experience = models.PositiveIntegerField()
-    education = models.CharField(max_length=70)
-    specialization = models.CharField(max_length=70)
-    contact_information = models.CharField(max_length=70)
-    profile_photo = models.ImageField(
-        upload_to='profile_photos', null=True, blank=True)
-
-    users = models.ManyToManyField(User, through='SpecialistUser')
+    id = models.BigAutoField(primary_key=True)
+    experience = models.TextField()
+    education = models.TextField()
+    contacts = models.TextField()
+    about = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=64)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    edit_dt = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Специалист'
         verbose_name_plural = 'Специалисты'
 
     def __str__(self):
-        return f"{self.surname} {self.first_name}"
+        return f"{self.id}"
 
 
 class SpecialistUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE)
-    role = models.CharField(choices=(SPECIALIST_ROLE_CHOICES))
+    user = models.ForeignKey(DemographicUser, on_delete=models.CASCADE, related_name='specialist_user')
+    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE, related_name='specialist_user')
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='specialist_user')
     specialization = models.ForeignKey(
-        Specialization, on_delete=models.CASCADE, null=True, blank=True)
+        Specialization, on_delete=models.CASCADE, null=True, blank=True, related_name='specialist_user')
 
     class Meta:
         verbose_name = 'Специалист-Пользователь'
