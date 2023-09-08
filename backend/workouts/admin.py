e7d97aca190e0a5e38dc8f680af2456b47007434
Nikita Lukyanchuk <nikluk@mail.ru>
2023-09-08 16:50:18 +0300

    register models in admin.py

--- backend/workouts/admin.py
+++ backend/workouts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (Exercise,
+                     ExercisesList,
+                     Exercises_list_Exercise,
+                     Training,
+                     TrainingPlan,
+                     TrainingType)
+
+admin.site.register(Exercise)
+admin.site.register(Training)
+admin.site.register(TrainingPlan)
+admin.site.register(TrainingType)
+admin.site.register(Exercises_list_Exercise)
+admin.site.register(ExercisesList)
 
 # Register your models here.
