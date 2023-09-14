d61fd9ee94bd6a56f19d76948c3d6912cb6f1cec
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-09-14 13:56:52 +0300

    fix_5: view all meal plans

--- backend/diets/urls.py
+++ backend/diets/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DietPlanViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(r'dietplans', DietPlanViewSet, basename='dietplans')
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]
+
