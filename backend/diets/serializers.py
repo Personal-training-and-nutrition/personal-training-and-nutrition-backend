2e5e53e5b278f6530a94ef9fba69af0aabcfbd1b
Kirill <116078844+KirillRumyantsev@users.noreply.github.com>
2023-09-16 17:39:48 +0300

    fix_5: view all meal plans (#25)

---
+++ backend/diets/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import DietPlan
+
+
+class DietPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DietPlan
+        fields = ('id', 'user', 'describe ', 'create_dt')
