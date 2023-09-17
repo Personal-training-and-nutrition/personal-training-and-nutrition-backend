a5ff5b682d7c2e3f1afef3c2b88c04fabc28c035
Nikita Lukyanchuk <nikluk@mail.ru>
2023-09-18 01:20:47 +0300

    realise CRUD for users (diet-plans)

--- backend/diets/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import DietPlan
-
-
-class DietPlanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DietPlan
-        fields = ('id', 'user', 'describe ', 'create_dt')
