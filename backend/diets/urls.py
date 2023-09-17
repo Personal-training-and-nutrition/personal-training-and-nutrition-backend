3b74d39fe07a4bd2917304953a4a7df59813d9af
Nikita Lukyanchuk <nikluk@mail.ru>
2023-09-17 13:47:20 +0300

    Feature update training plan (#29)
    * Bugfix flake8 and migrations
    
    * Update training plan through put request
    
    * Bugfix (Update training plan through put request)

--- backend/diets/urls.py
+++ backend/diets/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import DietPlanViewSet
 
 router_v1 = DefaultRouter()
@@ -8,4 +9,3 @@ router_v1.register(r'dietplans', DietPlanViewSet, basename='dietplans')
 urlpatterns = [
     path('', include(router_v1.urls)),
 ]
-
