34e9b54a9ae2a0991f2ba372caaf53ed8d063c2b
Stanislav <zo22@mail.ru>
2023-10-19 19:05:35 +0400

    first time wasn't enough to fix code to flake8 standarts

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+
 # from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin,)
