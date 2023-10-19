c97b8c41c87454d49b2a39592ebc50dd3a35f2a9
Stanislav <zo22@mail.ru>
2023-10-19 18:57:27 +0400

    changed _create_user method from make_password to set_password

--- backend/users/models.py
+++ backend/users/models.py
@@ -142,7 +142,7 @@ class UserManager(BaseUserManager):
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.password = make_password(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
