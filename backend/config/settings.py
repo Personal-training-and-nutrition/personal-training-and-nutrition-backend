9c57028b9ed5c4ceceafe096671f9e2a2a401650
msavilov <caba7@mail.ru>
2023-11-11 02:11:32 +0300

    correct link for reset_password

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -267,7 +267,7 @@ DJOSER = {
     # 'SEND_CONFIRMATION_EMAIL': True,
     'SET_PASSWORD_RETYPE': True,
     # 'ACTIVATION_URL': 'api/activate/{uid}/{token}',
-    'PASSWORD_RESET_CONFIRM_URL': 'reset_password?uid={uid}&token={token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'reset-password/?uid={uid}&token={token}',
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
     'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': [
         'https://connect.mail.ru',
